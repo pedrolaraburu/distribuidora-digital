49691f226ae8f701a2acd68b4493c16f2ec4ac00
Pedro Laraburu <pedrolaraburu@gmail.com>
2022-11-09 15:13:48 -0300

    Criando app core

--- projeto_dd/core/models.py
+++ projeto_dd/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class TimeStampedModelo(models.Model):
+class TimeStampedModel(models.Model):
     created = models.DateTimeField(
         'criado em',
         auto_now_add=True,

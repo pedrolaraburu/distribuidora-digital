583b6c790661e0b4fc31804735d2ba98b890b48a
Pedro Laraburu <pedrolaraburu@gmail.com>
2022-11-26 17:18:28 -0300

    Estoque saida form

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -11,7 +11,6 @@ def estoque_entrada_list(request):
     context = {'object_list': objects}
     return render(request, template_name, context)
 
-
 def estoque_entrada_detail(request, pk):
     template_name = 'estoque_entrada_detail.html'
     obj = Estoque.objects.get(pk=pk) 
@@ -45,6 +44,33 @@ def estoque_entrada_add(request):
             formset.save()
             dar_baixa_estoque(form)
             url='estoque:estoque_entrada_detail'
+            return HttpResponseRedirect(resolve_url(url, form.pk))
+    else:
+        form= EstoqueForm(instance=estoque_form, prefix='main')
+        formset=item_estoque_formset(instance=estoque_form, prefix='estoque')
+
+    context={'form': form, 'formset': formset}
+    return render(request, template_name, context)
+
+def estoque_saida_add(request):
+    template_name = 'estoque_saida_form.html'
+    estoque_form=Estoque()
+    item_estoque_formset= inlineformset_factory(
+        EstoqueSaida,
+        EstoqueItens,
+        form=EstoqueItensForm,
+        extra=0,
+        min_num=1,
+        validate_min=True,
+    )
+    if request.method == 'POST':
+        form= EstoqueForm(request.POST, instance=estoque_form, prefix='main')
+        formset=item_estoque_formset(request.POST, instance=estoque_form, prefix='estoque')
+        if form.is_valid() and formset.is_valid():
+            form=form.save()
+            formset.save()
+            dar_baixa_estoque(form)
+            url='estoque:estoque_saida_detail'
             return HttpResponseRedirect(resolve_url(url, form.pk))
     else:
         form= EstoqueForm(instance=estoque_form, prefix='main')

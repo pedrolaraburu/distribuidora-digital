a9116394852cbda3b0bfe608a87a14b6b29c0bbe
Pedro Laraburu <pedrolaraburu@gmail.com>
2022-11-26 11:38:33 -0300

    Entrada estoque add

--- projeto/settings.py
+++ projeto/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Aplicativos de terceiros
     'widget_tweaks',
+    'bootstrapform',
     # Meus apps
     'projeto.core',
     'projeto.produto',
